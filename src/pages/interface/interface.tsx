39fe29569736fcc46687f275b28f5a851f80402e
jiexinl <jiexinl@geo-k.cn>
2021-10-20 10:16:01 +0800

    commit

---
+++ src/pages/interface/interface.tsx
@@ -0,0 +1,6 @@
+export type Users = {
+  uid: number;   //用户id
+  username: String;    //账户
+  password: String;     //密码
+  uname: String;    //用户名称
+};
--- src/pages/user/usersInfo.tsx
+++ src/pages/user/usersInfo.tsx
@@ -1,30 +1,63 @@
-import * as React from 'react';
-import { login } from '@/net/service';
-import { history } from '@@/core/history';
-import styles from './usersInfo.less';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Table, Tag, Space } from 'antd';
+import React from 'react';
+import { Users } from '@/pages/interface/interface';
+import styles from '@/pages/user/usersInfo.less';
 
-class  UsersInfo extends  React.Component<any, any>{
+class UsersInfo extends React.Component<any, any> {
+  columns = [
+    {
+      title: '用户id',
+      dataIndex: 'uid',
+      key: 'uid'
+    },
+    {
+      title: '账号',
+      dataIndex: 'username',
+      key: 'username',
+    },
+    {
+      title: '用户名称',
+      dataIndex: 'uname',
+      key: 'uname',
+    },
+    {
+      title: '操作',
+      key: 'action',
+      render: () => (
+        <Space size="middle">
+          <a>修改</a>
+          <a>删除</a>
+        </Space>
+      ),
+    },
+  ];
+
+  data = [
+    {
+      key: '1',
+      uid: 'id1',
+      username: '111',
+      uname: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      uid: 'id2',
+      username: '111',
+      uname: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      uid: 'id2',
+      username: '111',
+      uname: 'Sidney No. 1 Lake Park'
+    },
+  ];
 
   render() {
-    return(
-      <div className={styles.div1}>
-        <div className={styles.div2}>
-          <table className={styles.table}>
-            <tr className={styles.tr}>
-              <th className={styles.th}>用户id</th>
-              <th className={styles.th}>账号</th>
-              <th className={styles.th}>用户名称</th>
-            </tr>
-            <tr className={styles.tr}>
-              <td className={styles.td}>222</td>
-              <td className={styles.td}>222</td>
-              <td className={styles.td}>222</td>
-            </tr>
-          </table>
-        </div>
-      </div>
+    return (
+      <Table columns={this.columns} dataSource={this.data} className={styles.table}/>
     );
   }
-}
+};
+
 export default UsersInfo;
